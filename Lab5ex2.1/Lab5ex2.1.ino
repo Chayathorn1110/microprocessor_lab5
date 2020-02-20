b63f1e43e928c81b21445ce00734550d51507191
Kenchayathorn <62011110@kmitl.ac.th>
2020-02-20 15:12:49 +0700

    microprocessor_lab5
    In task2
    RTC and normal delay are equal, RTC based on real time and normal delay counting on seconds which both cases are identical .

---
+++ Lab5ex2.1/Lab5ex2.1.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, HIGH);
+  Serial.println("Start");
+}
+
+void loop() {
+  int Secs = 0;
+  for(Secs = 0; Secs <=120; Secs++)
+  {
+    if(Secs == 0 || Secs <= 10)
+    {
+      digitalWrite(13,Secs%2);
+      delay(1000);
+    }
+  }
+  Serial.println("STOP");
+  while(true);
+
+}
